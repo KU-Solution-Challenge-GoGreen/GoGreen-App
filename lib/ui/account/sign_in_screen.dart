1b9907c50bcc2c5e7e8f5017d67480335c15e552
Chaeyoung Ha <81142151+ChaeyoungHa@users.noreply.github.com>
2023-03-30 12:18:04 +0900

    fix: change the position of SignInButton

--- lib/ui/account/sign_in_screen.dart
+++ lib/ui/account/sign_in_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gogreen_client/constants/colors.dart';
+import 'package:gogreen_client/constants/dimens.dart';
 
 import '../../controller/user_controller.dart';
 
@@ -11,28 +12,28 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.Green,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Column(
-            children: <Widget>[
-              SvgPicture.asset(
-                'assets/images/logo.svg',
-                semanticsLabel: 'logo',
-              ),
-              Container(
-                width: double.infinity,
-              )
+        backgroundColor: AppColors.Green,
+        body: SafeArea(
+            child: SizedBox(
+          width: scaleWidth(context) * 360,
+          height: scaleHeight(context) * 800,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(top: scaleHeight(context) * 289),
+                  child: SvgPicture.asset(
+                    'assets/images/logo.svg',
+                    semanticsLabel: 'logo',
+                  )),
+              Padding(
+                  padding: EdgeInsets.only(top: scaleHeight(context) * 266),
+                  child: SignInButton(Buttons.Google, onPressed: () {
+                    signInWithGoogle();
+                    //Navigator.pushNamed(context, '/setnick_or_dailymeal');
+                  })),
             ],
           ),
-          SignInButton(Buttons.Google, onPressed: () {
-            signInWithGoogle();
-            //Navigator.pushNamed(context, '/setnick_or_dailymeal');
-          }),
-        ],
-      ),
-    );
+        )));
   }
 }
